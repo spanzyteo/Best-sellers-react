e2a2dc6e4249bd8a73d794d9b96fc1a52648774a
spanzy teo <spanzytheo@gmail.com>
2023-08-26 06:25:32 +0100

    best sellers react
    description

---
+++ src/books.js
@@ -0,0 +1,25 @@
+import img1 from './images/book-2.jpg'
+import img2 from './images/book-3.jpg'
+import img3 from './images/book-4.jpg'
+
+export const books = [
+  {
+    author: 'Don Miguel Ruiz',
+    title: 'A Practical Guide to Personal Freedom (A Toltec Wisdom Book)',
+    img: img1,
+    id: 1,
+  },
+  {
+    author: ' Bessel van der Kolk M.D. ',
+    title:
+      'The Body Keeps the Score: Brain, Mind, and Body in the Healing of Traumae ',
+    img: img2,
+    id: 2,
+  },
+  {
+    author: 'Rebecca Yarros ',
+    title: 'Iron Flame (The Empyrean, 2) ',
+    img: img3,
+    id: 3,
+  },
+]
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { books } from './books'
+import Book from './Book'
+
+const BookList = () => {
+  return (
+    <>
+      <h1>Amazon best sellers</h1>
+      <section className="bookList">
+        {books.map((book, index) => {
+          return <Book {...book} key={book.id} number={index} />
+        })}
+      </section>
+    </>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<BookList />)
